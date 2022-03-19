dfad40b88ba0deb772ad128319f4c6be9cc4d17d
Alexander Lansdorp <alexlansdorprs@gmail.com>
2022-03-19 16:03:08 -0500

    Festival section added

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
       <Header />
       <AboutUs />
       <FairSection/>
-      <TouristPlanSection />
+      {/* <TouristPlanSection /> */}
     </React.Fragment>
   );
 }
--- src/Components/FairSection.jsx
+++ src/Components/FairSection.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import lgbt1 from '../Assets/lgbt1.jpeg';
+import lgbt2 from '../Assets/lgbt2.jpeg';
+import lgbt3 from '../Assets/lgbt3.jpeg';
+import lgbt4 from '../Assets/lgbt4.jpeg';
+import lgbt5 from '../Assets/lgbt5.jpeg';
 
 export default function FairSection() {
   return (
@@ -10,7 +15,7 @@ export default function FairSection() {
       >
         <div className="carousel-inner">
           <h1 className="sectionTitle">
-            Feria "Reflejando la luz del arcoíris"
+            Festival "Reflejando la luz del arcoíris"
           </h1>
           <div className="carousel-item active" data-bs-interval="10000">
             <p className="image--info">
@@ -18,11 +23,30 @@ export default function FairSection() {
               se sentiran libres de expresar sus gustos, identidad de genero y
               demostrar su creatividad y estilo en diferentes actividades.
             </p>
-            <img
-              src="http://fasab.udistrital.edu.co:8080/image/image_gallery?uuid=18f1c661-c406-4377-9ece-69e64486b16c&groupId=98864&t=1479244441532"
-              className="d-block"
-              alt="..."
-            />
+            <img src={lgbt1} className="d-block" alt="..." />
+          </div>
+          <div className="carousel-item" data-bs-interval="10000">
+            <p className="image--info">
+              Este festival cultural tendra lugar todos los años el 10 de
+              septiembre en el parque de los hippies (Calle 60 Av Kr 7 Bogotá)
+            </p>
+            <img src={lgbt5} className="d-block" alt="..." />
+          </div>
+          <div className="carousel-item" data-bs-interval="10000">
+            <p className="image--info">
+              En busca de promover la diversidad en general, nos enfocaremos en
+              fometar el respeto, igualdad, aceptacion y empatia, creando asi un
+              espacio seguro y sano en el cual compartiremos y disfrutaremos
+              como comunidad.
+            </p>
+            <img src={lgbt3} className="d-block" alt="..." />
+          </div>
+          <div className="carousel-item" data-bs-interval="10000">
+            <p className="image--info">
+              Entre las actividades que desarrollaremos habra desfiles, musica, muestras culturales,
+              gastronomia y un gran variedad de productos ofrecidos por emprendedores y comerciantes locales.
+            </p>
+            <img src={lgbt4} className="d-block" alt="..." />
           </div>
         </div>
         <button
