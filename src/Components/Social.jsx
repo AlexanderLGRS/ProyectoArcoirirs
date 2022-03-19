a149922bdb2a3e405ed5882fb7de77ad18e89afb
Alexander Lansdorp <alexlansdorprs@gmail.com>
2022-03-19 17:16:39 -0500

    Social section added

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -5,13 +5,28 @@ export default function Social() {
   return (
     <React.Fragment>
       <div className="container">
-        <a className='socialLink facebook' target="_blank" href="https://www.facebook.com/Proyecto-Arcoiris-102493169079085/">
+        <a
+          className="socialLink facebook"
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.facebook.com/Proyecto-Arcoiris-102493169079085/"
+        >
           <ion-icon size="large" name="logo-facebook"></ion-icon>
         </a>
-        <a className='socialLink instagram' target="_blank" href="https://www.instagram.com/reflejo.arcoirirs/">
+        <a
+          className="socialLink instagram"
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.instagram.com/reflejo.arcoirirs/"
+        >
           <ion-icon size="large" name="logo-instagram"></ion-icon>
         </a>
-        <a className='socialLink whatsapp' target="_blank" href="https://www.instagram.com/reflejo.arcoirirs/">
+        <a
+          className="socialLink whatsapp"
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.instagram.com/reflejo.arcoirirs/"
+        >
           <ion-icon size="large" name="logo-whatsapp"></ion-icon>
         </a>
       </div>
